b4f32c0f1b1c3090f28a41aa8023810e9ef2b505
sttstania <sttstania@users.noreply.github.com>
2025-05-09 15:39:25 +0200

    corrections

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,9 +5,9 @@ import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 
 const routes: Routes = [
-  { path: '/', component: HomeComponent },
-  { path: '/about', component: AboutComponent },
-  { path: '/contact', component: ContactComponent },
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
   { path: '**', redirectTo: '' },   //toutes les routes non definies (404)
 ];
 
