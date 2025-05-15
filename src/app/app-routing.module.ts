2e0fb7b6fe48c226dafc1b799d3d2ff2cd68de1b
sttstania <sttstania@users.noreply.github.com>
2025-05-15 12:49:26 +0200

    page produit ts et html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { ProductComponent } from './pages/product/product.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'accueil', component: HomeComponent },
+  { path: 'product', component: ProductComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: '**', redirectTo: '' },   //toutes les routes non definies (404)
