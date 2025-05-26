20dac85e51bc283b44228f36e83d05a301f0532b
sttstania <sttstania@users.noreply.github.com>
2025-05-26 16:01:50 +0200

    carousel

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FigurineModel } from '../../models/figurine.model';
+import { FigurineService } from '../../services/figurines.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +10,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  figurines: FigurineModel[] = [];
+  visibleFigurines: FigurineModel[] = [];
+  currentFigurine = 0;
+  itemsPerPage = 3;
 
+  constructor(private figurineService: FigurineService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.figurineService.getFigurines().subscribe(data => {
+      this.figurines = data;
+      this.updateVisibleFigurines();
+    });
+  }
+
+  updateVisibleFigurines() {
+    const start = this.currentFigurine * this.itemsPerPage;
+    const end = start + this.itemsPerPage;
+    this.visibleFigurines = this.figurines.slice(start, end);
+  }
+
+  nextFigurine() {
+    if ((this.currentFigurine + 1) * this.itemsPerPage < this.figurines.length) {
+      this.currentFigurine++;
+      this.updateVisibleFigurines();
+    }
+  }
+  prevFigurine() {
+    if (this.currentFigurine > 0 ) {
+      this.currentFigurine--;
+      this.updateVisibleFigurines();
+    }
+  }
+
+  goToProduct(id: number) {
+    this.router.navigate(['/product', id]);
+  }
 }
